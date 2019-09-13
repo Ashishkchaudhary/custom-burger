5f5dee0bd67b1f2da5f6382103d5751f96b85e73
Ashishkchaudhary <ashish.93kumark@gmail.com>
2019-09-13 11:40:03 +0530

    Updates

---
+++ precache-manifest.b8cf9e5de124765825e6dbddcaef66dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c49263e0652d2b949b128310ffde944",
+    "url": "/custom-burger/index.html"
+  },
+  {
+    "revision": "08e32c5196bff78b57f9",
+    "url": "/custom-burger/static/css/main.b9049004.chunk.css"
+  },
+  {
+    "revision": "1c26b6290a5158e650a2",
+    "url": "/custom-burger/static/js/2.067f03da.chunk.js"
+  },
+  {
+    "revision": "08e32c5196bff78b57f9",
+    "url": "/custom-burger/static/js/main.bc07139a.chunk.js"
+  },
+  {
+    "revision": "acd11c373b99944d3c45",
+    "url": "/custom-burger/static/js/runtime~main.e22402e3.js"
+  }
+]);
